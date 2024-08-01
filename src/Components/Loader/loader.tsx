a952b83d110ca5db44811d0551b525ca4f5420e0
samshar123 <samsharsachu1@gmail.com>
2024-08-01 12:06:17 +0530

    loader animation added

--- src/Components/Loader/loader.tsx
+++ src/Components/Loader/loader.tsx
@@ -1,9 +1,37 @@
-
+import "./loader.css"
+import imgload from "../../../public/Img/imgloader2.png"
 
 const loader = () => {
   return (
-    <div>
-      <h1>loading</h1>
+    <div className="loader">
+      
+
+<div id="hearts-alpaca" className="hearts">
+ <div className="heart"></div>
+ <div className="heart"></div>
+ <div className="heart"></div>
+ <div className="heart"></div>
+ <div className="heart"></div>
+ <div className="heart"></div>
+ <div className="heart"></div>
+ <div className="heart"></div>
+ <div className="heart"></div>
+ <div className="heart"></div>
+ <div className="heart"></div>
+</div>
+
+
+      <div className="loadercenter">
+        <div className="loaderimg">
+          <img className="imgload" src={imgload} alt=""  />
+
+        </div>
+        <div className="loadercontent">
+          <h5>In The Name Of Allah..</h5>
+
+        </div>
+
+      </div>
     </div>
   )
 }
